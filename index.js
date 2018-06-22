1c7a9cdb7aca4ff462aef7200c186c4610d40e40
Hannah Shine <hshine@visitationacademy.org>
2018-06-22 13:39:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ function getFirstSelector(selector){
   return document.querySelector(selector);
 }
 
+function nestedTarget(){
+  return document.getElementById("nested").querySelector('div.target')
+}
